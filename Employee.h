fe24c55be132157dd08e3eefd5b3e4a7a96ccbd6
walkdis <walkdis.as@gmail.com>
2015-04-06 00:47:59 +0300

    Update and rename Sotr.h to Employee.h

---
+++ Employee.h
@@ -0,0 +1,43 @@
+// Copyright 2015 <Anna Simakova>
+#pragma once
+#include <stdio.h>
+#include <iostream>
+#include <string>
+using ::std::string;
+using ::std::cin;
+using ::std::cout;
+using ::std::ostream;
+
+class Employee {
+  string sur;
+  string job;
+  double salary;
+  double prem;
+  bool rab;
+  bool chief;
+
+ public:
+  Employee();
+  Employee(const string&, const string&, const bool, const double);
+  void ChD(const string&);
+  Employee(double);
+  Employee operator -(const double);
+  Employee operator +(const double);
+  friend ostream& operator<<(ostream&, const Employee&);
+  ~Employee();
+  string GetSur() const;
+  void SetSur(const string&);
+  string GetJob() const;
+  void SetJob(const string&);
+  double GetSalary() const;
+  void SetSalary(const double);
+  double GetPrem() const;
+  void SetPrem(const double);
+  bool IsChief() const;
+  void SetChief(const bool);
+  bool GetRab() const;
+  void SetRab(const bool);
+  Employee(const Employee&);
+  friend bool operator ==(Employee, const Employee);
+  void operator =(const Employee&);
+};

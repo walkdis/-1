9ac820f78bb4694d26a1c254af4e1d153f577a1f
walkdis <walkdis.as@gmail.com>
2015-04-06 00:49:47 +0300

    Create Employee.cpp

---
+++ Employee.cpp
@@ -0,0 +1,89 @@
+// Copyright 2015 <Anna Simakova>
+#include "stdafx.h"
+#include <string>
+#include "Sotr.h"
+
+
+Employee::Employee() {
+  sur = "";
+  job = "";
+  rab = true;
+}
+Employee::Employee(const string& ty, const string& dol,
+const bool qw, const double lop) {
+  sur = ty;
+  job = dol;
+  chief = qw;
+  salary = lop;
+  rab = true;
+}
+Employee::Employee(double l) : salary(l) {}
+void Employee::ChD(const string& job) {
+  this->job = job;
+}
+Employee Employee::operator-(const double k) {
+  return (salary - k);
+}
+Employee Employee::operator+(const double k) {
+  return (salary + k);
+}
+Employee::~Employee() {}
+
+string Employee::GetSur() const {
+  return sur;
+}
+void Employee::SetSur(const string& as) {
+  sur = as;
+}
+string Employee::GetJob() const {
+  return job;
+}
+void Employee::SetJob(const string& as) {
+  job = as;
+}
+double Employee::GetSalary() const {
+  return salary;
+}
+void Employee::SetSalary(const double as) {
+  salary = as;
+}
+double Employee::GetPrem() const {
+  return prem;
+}
+void Employee::SetPrem(const double as) {
+  prem = as;
+}
+bool Employee::IsChief() const {
+  return chief;
+}
+void Employee::SetChief(const bool as) {
+  chief = as;
+}
+bool Employee::GetRab() const {
+  return rab;
+}
+void Employee::SetRab(const bool as) {
+  rab = as;
+}
+Employee::Employee(const Employee& b) {
+  sur = b.sur;
+  job = b.job;
+  salary = b.salary;
+  prem = b.prem;
+  rab = b.rab;
+  chief = b.chief;
+}
+bool operator ==(Employee df, const Employee qw) {
+  if (df == qw)
+  return true;
+  else
+  return false;
+}
+void Employee::operator =(const Employee& b) {
+  sur = b.sur;
+  job = b.job;
+  salary = b.salary;
+  prem = b.prem;
+  rab = b.rab;
+  chief = b.chief;
+}

df06fc674e6d0ce75f473573ea1ff2f7ed8fb59d
walkdis <walkdis.as@gmail.com>
2015-05-11 18:40:50 +0300

    Update AlrExist.cpp

--- AlrExist.cpp
+++ AlrExist.cpp
@@ -1,4 +1,5 @@
 // Copyright 2015 <Anna Simakova>
+#include "stdafx.h"
 #include "AlrExist.h"
 #include <string>
 
@@ -6,6 +7,6 @@
 EmployeeAlrExist::EmployeeAlrExist(const string& name) :exception() {
     msg = "Employee: " + name + " already exist";
 }
-const char * EmployeeAlrExist::what() const noexcept(true) {
+const char * EmployeeAlrExist::what() const throw() {
     return msg.c_str();
 }
